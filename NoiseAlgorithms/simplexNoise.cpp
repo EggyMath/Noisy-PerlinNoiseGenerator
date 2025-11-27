b77a21269179b18bf25a4b0a1f90fe51fe26a939
CMarink <christianm7765@gmail.com>
2025-11-26 20:50:31 -0500

    Added main implementation for simplex noise

--- NoiseAlgorithms/simplexNoise.cpp
+++ NoiseAlgorithms/simplexNoise.cpp
@@ -5,3 +5,104 @@ static const math::Vec2 gradients[8] = {
     { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }
 };
 
+simplexNoise::simplexNoise() : genericNoise() {}
+
+float simplexNoise::simplex(float x, float y) const {
+    const float F2 = 0.366025403f; // (sqrt(3) - 1) / 2
+    const float G2 = 0.211324865f; // (3 - sqrt(3)) / 6
+
+    // Convert (x, y) to triangular grid cell coordinates
+    float s = (x + y) * F2;
+    int i = std::floor(x + s);
+    int j = std::floor(y + s);
+
+    // Unskew
+    float t = (i + j) * G2;
+
+    float x0 = x - (i - t);
+    float y0 = y - (j - t);
+
+    int i1;
+    int j1;
+
+    // Determine which triangle half
+    if (x0 > y0) {
+        i1 = 1;
+        j1 = 0;
+    }
+    else {
+        i1 = 0; 
+        j1 = 1;
+    }
+
+    // Calculate three triangle corners
+    float x1 = x0 - i1 + G2;
+    float y1 = y0 - j1 + G2;
+    float x2 = x0 - 1.0f + 2.0f * G2;
+    float y2 = y0 - 1.0f + 2.0f * G2;
+
+    int ii = i & 255;
+    int jj = j & 255;
+
+    // hash corner positions into gradiants
+    int gi0 = perm[ii + perm[jj]] % 8;
+    int gi1 = perm[ii + i1 + perm[jj + j1]] % 8;
+    int gi2 = perm[ii + 1 + perm[jj + 1]] % 8;
+
+    float n0, n1, n2;
+
+    // calculate influence of each corner
+    float t0 = 0.5f - x0*x0 - y0*y0;
+
+    if (t0 < 0) {
+        n0 = 0.0f;
+    }
+    else {
+        t0 = t0 * t0;
+        n0 = t0 * t0 * dot(gradients[gi0], x0, y0);
+    }
+
+    float t1 = 0.5f - x1*x1 - y1*y1;
+
+    if (t1 < 0) {
+        n1 = 0.0f;
+    }
+    else {
+        t1 = t1 * t1;
+        n1 = t1 * t1 * dot(gradients[gi1], x1, y1);
+    }
+
+    float t2 = 0.5f - x2*x2 - y2*y2;
+
+    if (t2 < 0) {
+        n2 = 0.0f;
+    }
+    else {
+        t2 = t2 * t2;
+        n2 = t2 * t2 * dot(gradients[gi2], x2, y2);
+    }
+
+    // Scale to [0, 1]
+    return (70.0f * (n0 + n1 + n2)) * 0.5f + 0.5f;
+}
+
+// Creates the permutation table (pseudo-random)
+void simplexNoise::generatePermutation(unsigned int seed)  {
+    std::vector<int> p(256);
+
+    for (int i = 0; i < 256; i++) {
+        p[i] = i;
+    }
+
+    std::mt19937 rng(seed);
+    std::shuffle(p.begin(), p.end(), rng);
+
+    for (int i = 0; i < 512; i++) {
+        perm[i] = p[i & 255];
+    }
+}
+
+float simplexNoise::dot(const math::Vec2& g, float x, float y) const {
+    return g.x * x + g.y * y;
+}
+
